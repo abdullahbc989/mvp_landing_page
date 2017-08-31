4cc4b2cbaf4d0945eae599612b9df3865c15902f
Abdullah Clute <Abdullah.Clute@tdstelecom.com>
2017-08-31 08:58:20 -0500

    Modified form

--- src/newsletter/forms.py
+++ src/newsletter/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.forms import ValidationError
+from django.http import HttpResponse
 from .models import SignUp
 
 
@@ -7,6 +8,27 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = [
-            'email',
             'full_name',
+            'email',
         ]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+
+        domain, extension = provider.split('.')
+
+        domains = ['gmail', 'outlook', 'yahoo']
+        extensions = ['edu', 'com', 'org']
+
+        if domain not in domains:
+            raise ValidationError("Please use an accepted email domain")
+        if extension not in extensions:
+            raise ValidationError("Please use an accepted email extension")
+
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+
+        return full_name
