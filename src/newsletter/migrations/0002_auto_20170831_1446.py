57ac6fb07f5e8da7f90af0e3d1ae6fd48ef187b0
Abdullah Clute <Abdullah.Clute@tdstelecom.com>
2017-08-31 09:52:32 -0500

    Created model form and updated Homepage view

---
+++ src/newsletter/migrations/0002_auto_20170831_1446.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('newsletter', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='signup',
+            name='email',
+            field=models.EmailField(unique=True, max_length=254),
+        ),
+        migrations.AlterField(
+            model_name='signup',
+            name='full_name',
+            field=models.CharField(max_length=120, null=True),
+        ),
+    ]
--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,11 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import SignUpForm
+from .models import SignUp
+
 
 # Create your views here.
 
 
 def home(request):
-    context = {}
+    form = SignUpForm(request.POST or None)
+    signup_title = "Sign Up Here"
+
+    receivers = SignUp.objects.all()
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        signup_title = "Thank you!"
+
+    context = {
+        "form": form,
+        "receivers": receivers,
+        "signup_title": signup_title,
+    }
     template = "home.html"
     return render(request, template, context)
